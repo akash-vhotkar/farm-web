bc5a8f5b2a1a80d3b37219bff7675f563e8f0020
akash127 <akash@iotric.com>
2022-04-27 01:06:50 +0530

    done

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const cors=require('cors')
 const errorMiddleware = require("./middleware/error")
 
 app.use(express.json());
-app.use(cors({origin:"http://localhost:3000"} ))
+app.use(cors({origin:"*"} ))
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(fileUpload());
